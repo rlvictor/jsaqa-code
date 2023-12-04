bcc33c6664c0c5b18ce56a920274ddf17eb77ea7
RLVictor <vityad888@yandex.ru>
2023-12-04 20:44:31 +0300

    Initial commit

--- 7.3/playwright/NetologyTest.js
+++ 7.3/playwright/NetologyTest.js
@@ -3,8 +3,8 @@ const { chromium } = require("playwright");
 (async () => {
   const browser = await chromium.launch({
     headless: false,
-    slowMo: 5000,
-    devtools: true
+    slowMo: 500,
+    devtools: true,
   });
   const page = await browser.newPage();
   await page.goto("https://netology.ru");
